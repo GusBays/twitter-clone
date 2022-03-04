71d39be91dab5d49a2e65dc3124cb0326b091a25
Gustavo Bays <gustavo_boliv@hotmail.com>
2022-03-03 21:08:10 -0300

    feat: created timeline and functions to post tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -11,13 +11,45 @@ use MF\Model\Container;
 class AppController extends Action { 
 
     public function timeline() {
-        
+            
+        $this->validaAutenticacao();
+
+        //recuperar os tweets e encaminha pra view timeline
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        //cria um atributo dinamico na view que recebe os tweets do array
+        $this->view->tweets = $tweets;
+
+        $this->render('timeline');
+    }
+
+    public function tweet() {
+
+        $this->validaAutenticacao();
+            
+        //cria a instancia com a conexao do db feita
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweet->salvar();
+
+        header('Location: /timeline');
+    }
+
+    public function validaAutenticacao() {
+
         session_start();
 
-        if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-            $this->render('timeline');
-        } else {
+        if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['id']) || $_SESSION['id'] == '') {
             header('Location: /?login=erro');
+        } else {
+            
         }
     }
 }
